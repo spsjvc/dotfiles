51369266d0a6514ca24b416edc7d8011e9a83fe9
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2021-05-27 11:26:37 +0200

    Install vscode extensions in setup.sh

--- setup.sh
+++ setup.sh
@@ -10,3 +10,7 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     cp ./vscode/settings.json $HOME/.config/Code/User/settings.json
     cp ./vscode/keybindings.json $HOME/.config/Code/User/keybindings.json
 fi
+
+code --install-extension file-icons.file-icons
+code --install-extension esbenp.prettier-vscode
+code --install-extension tombonnike.vscode-status-bar-format-toggle
