f5ac0b20a69827f183c57b68d60b6fcf3dcdbb0d
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2021-05-27 11:15:16 +0200

    Update setup.sh to copy vscode settings

--- setup.sh
+++ setup.sh
@@ -2,3 +2,11 @@
 
 cp .gitconfig ~/.gitconfig
 cp .vimrc ~/.vimrc
+
+if [ "$(uname)" == "Darwin" ]; then
+    cp ./vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
+    cp ./vscode/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    cp ./vscode/settings.json $HOME/.config/Code/User/settings.json
+    cp ./vscode/keybindings.json $HOME/.config/Code/User/keybindings.json
+fi
