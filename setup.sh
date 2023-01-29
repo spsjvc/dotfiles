ba43d05769e49f21cbb05f8c36bb337bb54eb630
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2023-01-29 21:40:54 +0100

    Configure picom

--- setup.sh
+++ setup.sh
@@ -18,6 +18,9 @@ ln -s $DOTFILES_PATH/.config/i3/config $HOME/.config/i3/config
 mkdir -pv $HOME/.config/i3status
 ln -s $DOTFILES_PATH/.config/i3status/config $HOME/.config/i3status/config
 
+mkdir -pv $HOME/.config/picom
+ln -s $DOTFILES_PATH/.config/picom/picom.conf $HOME/.config/picom/picom.conf
+
 # Install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
