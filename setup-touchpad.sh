ec7ffc539b8c31064bfa9aeb2bb5162317202899
spsjvc <git@spsjvc.com>
2025-02-06 14:56:05 +0100

    Add touchpad settings

---
+++ setup-touchpad.sh
@@ -0,0 +1,9 @@
+# Set acceleration speed
+xinput set-prop 10 "libinput Accel Speed" 0.75
+
+# Enable two-finger right click
+xinput set-prop 10 "libinput Click Method Enabled" 0 1
+
+# Enable natural scrolling
+xinput set-prop 10 "libinput Natural Scrolling Enabled" 1
+
