4b1ebbc7d42318c6596c22ffdd768fa9b76bfe94
spsjvc <git@spsjvc.com>
2025-05-09 15:41:09 +0200

    Set touchpad device id dynamically

--- setup-touchpad.sh
+++ setup-touchpad.sh
@@ -1,9 +1,11 @@
+DEVICE_ID=$(xinput | grep -i 'touchpad' | grep -oP 'id=\K\d+')
+
 # Set acceleration speed
-xinput set-prop 10 "libinput Accel Speed" 0.75
+xinput set-prop $DEVICE_ID "libinput Accel Speed" 0.9
 
 # Enable two-finger right click
-xinput set-prop 10 "libinput Click Method Enabled" 0 1
+xinput set-prop $DEVICE_ID "libinput Click Method Enabled" 0 1
 
 # Enable natural scrolling
-xinput set-prop 10 "libinput Natural Scrolling Enabled" 1
+xinput set-prop $DEVICE_ID "libinput Natural Scrolling Enabled" 1
 
